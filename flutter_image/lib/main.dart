c4c337856b6c3e6efe80e0d8b4e55f0cc3c95a2b
hisurga <hisurga@gmail.com>
2019-06-19 00:40:59 +0900

    add FadeIn, CachedNetwork

--- flutter_image/lib/main.dart
+++ flutter_image/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_image/image_assets.dart';
 import 'package:flutter_image/image_url.dart';
+import 'package:flutter_image/image_fadeinimage.dart';
+import 'package:flutter_image/image_cachednetworkimage.dart';
 
 void main() => runApp(ImageApp());
 
@@ -12,8 +14,11 @@ class ImageApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyImagePage(title: 'Image from assets'),
+      // home: MyImagePage(title: 'Image from assets'),
       // home: MyImageUrlPage(title: 'Image from url'),
+      // home: MyImageFadeinPage(title: 'Image from url with placeholder'),
+      home:
+          MyImageCachednetworkPage(title: 'Image from url with cachednetwork'),
     );
   }
 }
